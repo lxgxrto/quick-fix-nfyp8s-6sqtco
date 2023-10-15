e7824d59399907cfe045ee4f6f0052b8f41e1f35
lxgxrto <lxgxrto@users.noreply.github.com>
2023-10-15 00:10:45 -0600

    Initial commit

---
+++ src/routes/Header.svelte
@@ -0,0 +1,31 @@
+<!-- <nav>
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+	<a href="/contact">Contact me</a>
+	<a href="/profile">My Profile</a>
+</nav> -->
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">QuickFix</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+					<a href="/" class="nav-link active">Home</a>
+        </li>
+        <li class="nav-item">
+					<a href="/about" class="nav-link">About</a>
+        </li>
+        <li class="nav-item">
+          <a href="/contact" class="nav-link">Contact me</a>
+        </li>
+        <li class="nav-item">
+          <a href="/profile" class="nav-link">My Profile</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
